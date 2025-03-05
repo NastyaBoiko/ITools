6cd29747bfb3c9b8d377dc49b17a130a81eed5a0
Nastya <nastya@mail.ru>
2025-03-05 11:15:04 +0300

    college git

--- config/db.php
+++ config/db.php
@@ -1,10 +1,23 @@
 <?php
 
+// return [
+//     'class' => 'yii\db\Connection',
+//     'dsn' => 'mysql:host=localhost;dbname=itools',
+//     'username' => 'root',
+//     'password' => '',
+//     'charset' => 'utf8',
+
+//     // Schema cache options (for production environment)
+//     //'enableSchemaCache' => true,
+//     //'schemaCacheDuration' => 60,
+//     //'schemaCache' => 'cache',
+// ];
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=itools',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=asfhpsun_m2',
+    'username' => 'asfhpsun',
+    'password' => 'GXsEYk',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
